b29eb308f4a02e5ad965e03c075e5791962a1ee1
thanh vinh <vinhthanhnguyen12122004@gmail.com>
2025-02-20 10:39:35 +0700

    video 22

--- src/Components/Mycomponent.js
+++ src/Components/Mycomponent.js
@@ -1,36 +1,10 @@
 import React from "react";
+import Userinfor from "./Userinfor";
 class MyComponent extends React.Component {
-  state = {
-    name: " thành vinh",
-    adress: "741hv",
-    age: 20,
-  };
-  handleOnClickbutton = (e) => {
-    console.log(e.target);
-  };
-  handleOncChangeinput = (e) => {
-    this.setState({
-      name: e.target.value,
-    });
-  };
-  handleOnSubmit = (e) => {
-    e.preventDefault();
-    alert("chặn rồi");
-  };
   render() {
-    let { name, adress, age } = this.state;
     return (
       <>
-        <div> tôi tên là {name}</div>
-        <button onClick={(e) => this.handleOnClickbutton(e)}>click me</button>
-        <form action="" onSubmit={(e) => this.handleOnSubmit(e)}>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => this.handleOncChangeinput(e)}
-          />
-          <button>submit</button>
-        </form>
+        <Userinfor />
       </>
     );
   }
