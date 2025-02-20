402edf6d52caff505239cad03d87e9d0ad487cbb
thanh vinh <vinhthanhnguyen12122004@gmail.com>
2025-02-20 09:52:36 +0700

    video -16

---
+++ src/Components/Mycomponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <>
+        <div> học lại class component</div>
+      </>
+    );
+  }
+}
+export default MyComponent;
