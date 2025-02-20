fee747f6efb4ac773ef6dfad3bf5cc3125bc5538
thanh vinh <vinhthanhnguyen12122004@gmail.com>
2025-02-20 21:03:07 +0700

    video 25

--- src/Components/Displayinfor.js
+++ src/Components/Displayinfor.js
@@ -1,19 +1,43 @@
 import React from "react";
 class Displayinfor extends React.Component {
+  state = {
+    check: false,
+  };
+
+  handleShowHide = () => {
+    this.setState({
+      check: !this.state.check,
+    });
+  };
+
   render() {
     let { listUsers } = this.props;
+    console.log(listUsers);
+    let { check } = this.state;
+
     return (
       <>
-        {listUsers.map((item, index) => {
-          return (
-            <>
-              <div key={item.id}>
-                tôi tên là {item.name} , tôi nam nay {item.age}
-                <hr />
-              </div>
-            </>
-          );
-        })}
+        {!check && <button onClick={() => this.handleShowHide()}>show</button>}
+
+        {check && (
+          <>
+            <div>list user:</div>
+            {listUsers.map((item, index) => {
+              return (
+                <>
+                  <div
+                    key={item.id}
+                    className={+item.age > 18 ? "green" : "red"}
+                  >
+                    tôi tên là {item.name} , tôi nam nay {item.age}
+                    <hr />
+                  </div>
+                </>
+              );
+            })}
+            <button onClick={() => this.handleShowHide()}>hide</button>
+          </>
+        )}
       </>
     );
   }
--- src/Components/Mycomponent.js
+++ src/Components/Mycomponent.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Userinfor from "./Userinfor";
+import Displayinfor from "./Displayinfor";
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "thành vinh", age: 16 },
+      { id: 2, name: "thành vinh", age: 26 },
+      { id: 3, name: "thành vinh", age: 20 },
+    ],
+  };
   render() {
     return (
       <>
         <Userinfor />
+        <Displayinfor listUsers={this.state.listUsers} />
       </>
     );
   }
