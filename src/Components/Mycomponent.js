58ab20df3c09614f6358fed1d41addf2ff1c8483
thanh vinh <vinhthanhnguyen12122004@gmail.com>
2025-02-20 10:24:52 +0700

    form

--- src/Components/Mycomponent.js
+++ src/Components/Mycomponent.js
@@ -5,10 +5,32 @@ class MyComponent extends React.Component {
     adress: "741hv",
     age: 20,
   };
+  handleOnClickbutton = (e) => {
+    console.log(e.target);
+  };
+  handleOncChangeinput = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+  handleOnSubmit = (e) => {
+    e.preventDefault();
+    alert("chặn rồi");
+  };
   render() {
+    let { name, adress, age } = this.state;
     return (
       <>
-        <div> tôi tên là {this.state.name}</div>
+        <div> tôi tên là {name}</div>
+        <button onClick={(e) => this.handleOnClickbutton(e)}>click me</button>
+        <form action="" onSubmit={(e) => this.handleOnSubmit(e)}>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => this.handleOncChangeinput(e)}
+          />
+          <button>submit</button>
+        </form>
       </>
     );
   }
